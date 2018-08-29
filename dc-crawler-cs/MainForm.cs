ff7c97e2db6f0fecc72d8b4346108c75b856de4e
rollrat <rollrat@naver.com>
2018-08-29 15:55:27 +0900

    ui: initialize

---
+++ dc-crawler-cs/MainForm.cs
@@ -0,0 +1,23 @@
+﻿/*************************************************************************
+
+   Copyright (C) 2018. rollrat All Rights Reserved.
+
+   Author: Jeong HyunJun
+
+***************************************************************************/
+
+namespace dc_crawler_cs
+{
+    public partial class MainForm : MetroFramework.Forms.MetroForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, System.EventArgs e)
+        {
+            Text += Version.Text;
+        }
+    }
+}
---
+++ dc-crawler-cs/Version.cs
@@ -0,0 +1,15 @@
+﻿/*************************************************************************
+
+   Copyright (C) 2018. rollrat All Rights Reserved.
+
+   Author: Jeong HyunJun
+
+***************************************************************************/
+
+namespace dc_crawler_cs
+{
+    public class Version
+    {
+        public const string Text = "0.1.0";
+    }
+}
