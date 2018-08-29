315bbfbf878fa427ed11af7d440da3c6948e2ead
rollrat <rollrat@naver.com>
2018-08-29 16:37:33 +0900

    dc: add selenium, test login and write

--- dc-crawler-cs/MainForm.Designer.cs
+++ dc-crawler-cs/MainForm.Designer.cs
@@ -29,26 +29,136 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.metroTabControl1 = new MetroFramework.Controls.MetroTabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroTabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // metroTabControl1
+            // 
+            this.metroTabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroTabControl1.Controls.Add(this.tabPage1);
+            this.metroTabControl1.Controls.Add(this.tabPage2);
+            this.metroTabControl1.Controls.Add(this.tabPage3);
+            this.metroTabControl1.Controls.Add(this.tabPage4);
+            this.metroTabControl1.Controls.Add(this.tabPage5);
+            this.metroTabControl1.Location = new System.Drawing.Point(10, 59);
+            this.metroTabControl1.Name = "metroTabControl1";
+            this.metroTabControl1.SelectedIndex = 0;
+            this.metroTabControl1.Size = new System.Drawing.Size(1136, 551);
+            this.metroTabControl1.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroTabControl1.TabIndex = 0;
+            this.metroTabControl1.UseSelectable = true;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.White;
+            this.tabPage1.Controls.Add(this.metroButton1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 38);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(1128, 509);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "게시글";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.White;
+            this.tabPage2.Location = new System.Drawing.Point(4, 38);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Size = new System.Drawing.Size(1128, 509);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "랭킹";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.BackColor = System.Drawing.Color.White;
+            this.tabPage3.Location = new System.Drawing.Point(4, 38);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(1128, 509);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "글작성";
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.BackColor = System.Drawing.Color.White;
+            this.tabPage4.Location = new System.Drawing.Point(4, 38);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(1128, 509);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "글삭제";
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.BackColor = System.Drawing.Color.White;
+            this.tabPage5.Location = new System.Drawing.Point(4, 38);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(1128, 509);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "설정";
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(678, 37);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(468, 19);
+            this.metroLabel1.TabIndex = 3;
+            this.metroLabel1.Text = "Copyright (C) 2018. DCInside Programming Gallery Union. All Rights Reserved.";
+            this.metroLabel1.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(500, 94);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(210, 46);
+            this.metroButton1.TabIndex = 0;
+            this.metroButton1.Text = "metroButton1";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1008, 409);
+            this.ClientSize = new System.Drawing.Size(1156, 616);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.metroTabControl1);
             this.Font = new System.Drawing.Font("맑은 고딕", 9F);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MinimumSize = new System.Drawing.Size(1156, 616);
             this.Name = "MainForm";
             this.Padding = new System.Windows.Forms.Padding(20, 75, 20, 25);
             this.Style = MetroFramework.MetroColorStyle.Green;
             this.Text = "DC Crawler ";
             this.Load += new System.EventHandler(this.MainForm_Load);
+            this.metroTabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroTabControl metroTabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroButton metroButton1;
     }
 }
 
--- dc-crawler-cs/MainForm.cs
+++ dc-crawler-cs/MainForm.cs
@@ -6,6 +6,11 @@
 
 ***************************************************************************/
 
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
 namespace dc_crawler_cs
 {
     public partial class MainForm : MetroFramework.Forms.MetroForm
@@ -18,6 +23,27 @@ namespace dc_crawler_cs
         private void MainForm_Load(object sender, System.EventArgs e)
         {
             Text += Version.Text;
+        }
+
+        private void metroButton1_Click(object sender, System.EventArgs e)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Url = "https://www.dcinside.com/";
+            driver.FindElement(By.Name("user_id")).SendKeys("qscsq1");
+            driver.FindElement(By.Name("password")).SendKeys("guswns1");
+            driver.FindElement(By.Id("login_ok")).Click();
+
+            for (int i = 0; i < 10; i++)
+            {
+                driver.Url = "http://gall.dcinside.com/board/write/?id=ani1_new1";
+                driver.FindElement(By.Name("subject")).SendKeys($"글쓰기 테스트 {i} ㅇㅅㅇ");
+                driver.SwitchTo().Frame(driver.FindElement(By.XPath("//iframe[@name='tx_canvas_wysiwyg']")));
+                driver.FindElement(By.TagName("body")).SendKeys("자동 글쓰기 테스트 중입니다.");
+                driver.SwitchTo().DefaultContent();
+                driver.FindElement(By.XPath("//input[@src='http://nstatic.dcinside.com/dgn/gallery/images/btn_save.gif']")).Click();
+                Thread.Sleep(1000);
+            }
         }
     }
 }
